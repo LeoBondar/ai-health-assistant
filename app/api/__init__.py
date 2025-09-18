4085156a0dfa128b0e6c64467e1cf33e27f8ddc8
Leonid Bondarenko <136648152+LeoBondar@users.noreply.github.com>
2025-09-18 14:32:23 +0300

    feat: upd routers

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -7,7 +7,7 @@ API_V1_PREFIX = "/api/v1"
 
 root_router = APIRouter()
 root_router.include_router(srv_router)
-root_router.include_router(chat_router)
+root_router.include_router(chat_router, prefix=API_V1_PREFIX)
 
 
 def init_router(app: FastAPI) -> None:
